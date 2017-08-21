1ab1e2cbe78341cde9eda295a530e0d0ff2b59c9
Anthony Ho <anthony.hyho@gmail.com>
2017-08-21 14:49:37 -0700

    new layout

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ Last updated 8/18/2017
 import numpy as np
 # import pandas as pd
 import dash
-#import dash_auth
+# import dash_auth
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
@@ -21,17 +21,23 @@ import lib
 
 
 # Define css links
-css_link = 'https://codepen.io/chriddyp/pen/bWLwgP.css'
+banner_link = 'https://raw.githubusercontent.com/anthonyho/utility_app/master/banner.png'
+css_links = ['https://fonts.googleapis.com/css?family=Overpass:300,300i',
+             'https://cdn.rawgit.com/plotly/dash-app-stylesheets/dab6f937fd5548cebf4c6dc7e93a10ac438f5efb/dash-technical-charting.css']
 
 # Define names and options
-dict_by = OrderedDict([('building_type', 'Building type'),
-                       ('cz', 'Climate zone')])
+list_types = ['Warehouse', 'Distribution',
+              'Office building', 'Medical building',
+              'Hospital / convalescent home', 'Hotel / motel',
+              'Shopping center', 'Department store / retail outlet',
+              'Food store / supermarket', 'Storefront retail',
+              'Miscell commercial']
 dict_iou = OrderedDict([('pge', 'PG&E'),
                         ('sce', 'SCE'),
                         ('scg', 'SCG'),
                         ('sdge', 'SDG&E')])
-dict_fuel1 = OrderedDict([('elec', 'Electric'),
-                          ('gas', 'Gas'),
+dict_fuel1 = OrderedDict([('elec', 'Electric only'),
+                          ('gas', 'Gas only'),
                           ('both', 'Both')])
 dict_fuel2 = OrderedDict([('tot', 'Total'),
                           ('elec', 'Electric'),
@@ -40,19 +46,14 @@ dict_unit = OrderedDict([('raw', 'Raw'),
                          ('EUI', 'EUI')])
 dict_stat = OrderedDict([('avg', 'Average annual total'),
                          ('fit', 'Trend')])
-list_types = ['Warehouse', 'Distribution',
-              'Office building', 'Medical building',
-              'Hospital / convalescent home',
-              'Hotel / motel',
-              'Shopping center', 'Department store / retail outlet',
-              'Food store / supermarket', 'Storefront retail',
-              'Miscell commercial']
+list_colorby = ['Building type', 'Climate zone',
+                'IOU', 'Fuel type',
+                'Consumption', 'Year built', 'Building area']
 
 # Define username and password
 # VALID_USERNAME_PASSWORD_PAIRS = [
 #     ['hello', 'world']
 # ]
-
 
 # Get options and arguments from command line
 description = 'Interactive web app for visualizing building energy data'
@@ -69,7 +70,7 @@ bills = lib.read_processed_bills(bills_file)
 
 # Compute names and options that are not defined in the section above
 # dynamically
-list_cz = [str(cz) 
+list_cz = [str(cz)
            for cz in np.sort(bills[('cis', 'cz')].unique().astype(int))]
 
 # Initiate dash
@@ -81,116 +82,155 @@ app = dash.Dash()
 # )
 
 
-# Define app components
-# Define radio botton and dropdown menu for "select by"
-by_radio = dcc.RadioItems(id='by_radio',
-                          options=[{'label': 'Building type',
-                                    'value': 'building_type'},
-                                   {'label': 'Climate zone', 'value': 'cz'}],
-                          value='cz',
+# Define app panel components
+
+# Define header
+header = html.Div([html.Img(src=banner_link,
+                            className='four columns',
+                            style={'height': '60',
+                                   'width': '200',
+                                   'float': 'left',
+                                   'position': 'relative'}),
+                   html.H3('Whole Building Consumption Browser',
+                           className='eight columns',
+                           style={'text-align': 'right'})],
+                  className='row')
+# Define filter components
+filter_types = dcc.Dropdown(id='filter_types',
+                            options=lib.to_options(list_types),
+                            value='Office building',
+                            multi=True)
+filter_cz = dcc.Checklist(id='filter_cz',
+                          options=lib.to_options(list_cz),
+                          values=list_cz,
                           labelStyle={'display': 'inline-block'})
-selection_dropdown = dcc.Dropdown(id='selection_dropdown')
-# Define radio botton for statistics to plot
-unit_radio = dcc.RadioItems(id='unit_radio',
-                            options=[{'label': 'Raw', 'value': 'raw'},
-                                     {'label': 'EUI', 'value': 'EUI'}],
-                            value='EUI',
-                            labelStyle={'display': 'inline-block'})
-# Define radio botton for statistics to plot
-stats_radio = dcc.RadioItems(id='stats_radio',
-                             options=[{'label': 'Average annual total',
-                                       'value': 'avg'},
-                                      {'label': 'Trend', 'value': 'fit'}],
-                             value='avg',
+filter_iou = dcc.Checklist(id='filter_iou',
+                           options=lib.to_options(dict_iou),
+                           values=list(dict_iou.keys()),
+                           labelStyle={'display': 'inline-block'})
+filter_fuel = dcc.RadioItems(id='filter_fuel',
+                             options=lib.to_options(dict_fuel1),
+                             value='both',
                              labelStyle={'display': 'inline-block'})
-# Define radio botton for fuel to plot
-fuel_radio = dcc.RadioItems(id='fuel_radio',
-                            options=[{'label': 'Total', 'value': 'tot'},
-                                     {'label': 'Electric', 'value': 'elec'},
-                                     {'label': 'Gas', 'value': 'gas'}],
-                            value='tot',
-                            labelStyle={'display': 'inline-block'})
+filter_value = dcc.RangeSlider(id='filter_value',
+                               min=0, max=1000, step=0.1,
+                               marks={i: str(i)
+                                      for i in range(0, 1000) if i % 100 == 0},
+                               value=[0, 1000])
+filter_year = dcc.RangeSlider(id='filter_year',
+                              min=2009, max=2015,
+                              marks={i: str(i) for i in range(2009, 2016)},
+                              value=[2009, 2015])
+filter_area = dcc.RangeSlider(id='filter_area',
+                              min=0, max=1000, step=0.1,
+                              marks={i: str(i)
+                                     for i in range(0, 1000) if i % 100 == 0},
+                              value=[0, 1000])
+
 # Define radio botton for color in map
-color_radio = dcc.RadioItems(id='color_radio',
-                             options=[{'label': 'Raw', 'value': 'raw'},
-                                      {'label': 'EUI', 'value': 'EUI'},
-                                      {'label': 'Climate zone', 'value': 'cz'},
-                                      {'label': 'Building type',
-                                       'value': 'building_type'}],
-                             value='EUI',
-                             labelStyle={'display': 'inline-block'})
+colorby = dcc.RadioItems(id='colorby',
+                         options=lib.to_options(list_colorby),
+                         value='Consumption',
+                         labelStyle={'display': 'inline-block'})
 
+# Define dropdown menu for metric
+metric_unit = dcc.Dropdown(id='metric_unit',
+                           options=lib.to_options(dict_unit),
+                           value='EUI')
+metric_fuel = dcc.Dropdown(id='metric_fuel',
+                           options=lib.to_options(dict_fuel2),
+                           value='tot')
+metric_stat = dcc.Dropdown(id='metric_stat',
+                           options=lib.to_options(dict_stat),
+                           value='avg')
 
-left_col_html = html.Div([html.Label('Slice data by:'),
-                          by_radio,
-                          selection_dropdown,
-                          html.Label('Plot data for: (to fix)'),
-                          unit_radio,
-                          html.Label('Statistics:'),
-                          stats_radio,
-                          html.Label('Fuel type:'),
-                          fuel_radio],
-                         style={'width': '48%',
-                                'display': 'inline-block'})
-right_col_html = html.Div([html.Label('Color buildings by:'),
-                           color_radio],
-                          style={'width': '48%',
-                                 'display': 'inline-block',
-                                 'float': 'right'})
-control_panel_html = html.Div([left_col_html,
-                               right_col_html],
-                              style={'borderBottom': 'thin lightgrey solid',
-                                     'backgroundColor': 'rgb(250, 250, 250)',
-                                     # 'padding': '10px 5px'
-                                     })
+# Define html subcomponents
+html_topleft = html.Div([html.H4('Filter buildings by:'),
+                         html.Label('Select building type:'),
+                         filter_types],
+                        className='eight columns')
+html_topright = html.Div([html.H4('Color buildings by:'),
+                          colorby],
+                         className='four columns')
+html_lowerleft = html.Div([html.Div([html.Label('Select climate zone:'),
+                                     filter_cz,
+                                     html.Label('Select IOU and fuel type:'),
+                                     filter_iou,
+                                     filter_fuel],
+                                    className='three columns'),
+                           html.Div([html.Label('Consumption range:'),
+                                     html.Div([filter_value],
+                                              style={'margin-bottom': '30'}),
+                                     html.Label('Year built:'),
+                                     html.Div([filter_year],
+                                              style={'margin-bottom': '30'}),
+                                     html.Label('Building area:'),
+                                     html.Div([filter_area],
+                                              style={'margin-bottom': '30'})],
+                                    className='five columns')])
+html_lowerright = html.Div([html.H4('Define consumption metric:'),
+                            html.Label('Unit:'),
+                            metric_unit,
+                            html.Label('Fuel:'),
+                            metric_fuel,
+                            html.Label('Statistics:'),
+                            metric_stat],
+                           className='four columns')
 
+app.layout = html.Div([header,
+                       html.Hr(style={'margin': '0', 'margin-bottom': '5'}),
+                       html.Div([html_topleft,
+                                 html_topright],
+                                className='row',
+                                style={'margin-bottom': '10'}),
+                       html.Div([html_lowerleft,
+                                 html_lowerright],
+                                className='row',
+                                style={'margin-bottom': '10'}),
+                       html.Div([html.Div([dcc.Graph(id='map')],
+                                          className='six columns'),
+                                 html.Div([dcc.Graph(id='boxplot')],
+                                          className='six columns')],
+                                className='row',
+                                style={'margin-bottom': '10'})],
+                      style={'width': '85%',
+                             'max-width': '1200',
+                             'margin-left': 'auto',
+                             'margin-right': 'auto',
+                             'font-family': 'overpass',
+                             'background-color': '#F3F3F3',
+                             'padding': '40',
+                             'padding-top': '20',
+                             'padding-bottom': '20'})
 
-# Define app layout
-app.layout = html.Div([html.Div([dcc.Graph(id='map')],
-                                style={'width': '49%',
-                                       'display': 'inline-block'}),
-                       html.Div([control_panel_html,
-                                 dcc.Graph(id='boxplot')],
-                                style={'width': '49%',
-                                       'display': 'inline-block',
-                                       'float': 'right'})
-                       ])
-app.css.append_css({"external_url": css_link})
-
-
-
-@app.callback(Output('selection_dropdown', 'options'),
-              [Input('by_radio', 'value')])
-def update_selection_options(by):
-    if by == 'building_type':
-        return [{'label': bldg_type, 'value': bldg_type}
-                for bldg_type in list_types]
-    elif by == 'cz':
-        return [{'label': 'Climate zone ' + cz, 'value': cz} for cz in list_cz]
-
-
-@app.callback(Output('selection_dropdown', 'value'),
-              [Input('by_radio', 'value')])
-def update_selection_value(by):
-    if by == 'building_type':
-        return 'Office building'
-    elif by == 'cz':
-        return '3'
-
+#app.css.append_css({"external_url": css_link})
+for css in css_links:
+    app.css.append_css({"external_url": css})
 
 @app.callback(Output('map', 'figure'),
-              [Input('by_radio', 'value')])
+              [Input('filter_iou', 'values')])
 def update_map(tmp):
     return lib.plot_map(bills)
 
 
 @app.callback(Output('boxplot', 'figure'),
-              [Input('by_radio', 'value'),
-               Input('selection_dropdown', 'value'),
-               Input('unit_radio', 'value'),
-               Input('stats_radio', 'value'),
-               Input('fuel_radio', 'value')])
-def update_boxplot(by, selection, unit, stats, fuel):
+              [Input('filter_types', 'value'),
+               Input('filter_cz', 'values'),
+               Input('filter_iou', 'values'),
+               Input('filter_fuel', 'value'),
+               Input('filter_value', 'value'),
+               Input('filter_year', 'value'),
+               Input('filter_area', 'value'),
+               Input('colorby', 'value'),
+               Input('metric_unit', 'value'),
+               Input('metric_fuel', 'value'),
+               Input('metric_stat', 'value')])
+def update_boxplot(tmp, tmp2, tmp3, tmp4, t5, t6, t7, t8, t9, t10, t11):
+    by = 'cz'
+    selection = '3'
+    unit = 'EUI'
+    stats = 'avg'
+    fuel = ''
     if by == 'cz':
         order = list_types
     else:
@@ -199,6 +239,10 @@ def update_boxplot(by, selection, unit, stats, fuel):
         value_suffix = '_slope'
     else:
         value_suffix = ''
+    by = 'cz'
+    selection = '3'
+    stats = 'avg'
+    fuel = 'tot'
 
     value = 'EUI_' + fuel + '_' + stats + '_2009_2015' + value_suffix
     return lib.plot_box(bills,
